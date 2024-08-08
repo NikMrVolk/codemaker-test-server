38e97f35d17c5064f2d14d75a15d0b57618e3e44
NickMozav <nick.mr.volk@gmail.com>
2024-08-08 11:28:21 +0300

    init

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+
+async function bootstrap() {
+  const port = process.env.PORT || 8000;
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: ['http://localhost:3000', 'http://159.223.237.230'],
+    credentials: true,
+    exposedHeaders: 'set-cookie',
+  });
+
+  await app.listen(port);
+}
+bootstrap();
